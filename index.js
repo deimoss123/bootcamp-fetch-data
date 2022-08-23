0b01860adf629951ec1c6778ac6fd872a922b8a0
DragonSky_NG <dragonskyng@gmail.com>
2022-08-23 18:11:14 +0300

    Add array of objects that include 'you'

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ import axios from "axios";
   // console.log(data);
 
   const uniqueTags = new Set();
+  const includesYou = [];
 
   for (const post of data.posts) {
     for (const tag of post.tags) {
       uniqueTags.add(tag);
     }
+    if (post.body.includes("you")) {
+      includesYou.push(post);
+    }
   }
 
   const uniqueTagsArray = Array.from(uniqueTags);
   console.log(uniqueTagsArray);
+  console.log(includesYou);
 })();
