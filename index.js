38244a43dd7aade7b59af92f9c42c0560b023c0a
deimoss123 <danielsalksnis@gmail.com>
2022-08-23 17:40:53 +0300

    print out unique tags

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ import axios from "axios";
 async function fetchData() {
   const response = await axios.get("https://dummyjson.com/posts");
   const { data } = response;
-  console.log(data);
+  // console.log(data);
+
+  const uniqueTags = new Set();
+  for (const post of data.posts) {
+    for (const tag of post.tags) {
+      uniqueTags.add(tag);
+    }
+  }
+
+  console.log(uniqueTags);
 }
 
 fetchData();
